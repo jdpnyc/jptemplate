74a659cf3ea695fa62b8811892800a91912cc36a
jdpnyc <jonathanpeters1@mac.com>
2023-06-25 10:06:27 -0400

    Initialize renovate/prettier-plugin-tailwindcss-0.x

---
+++ app/components/product/ProductHero.tsx
@@ -0,0 +1,42 @@
+import {Image} from '@shopify/hydrogen';
+import type {
+  Product,
+  ProductVariant,
+} from '@shopify/hydrogen/storefront-api-types';
+
+import ProductTile from '~/components/product/Tile';
+import {useGid} from '~/lib/utils';
+
+type Props = {
+  gid: string;
+  variantGid: string;
+};
+
+export default function ProductHero({gid, variantGid}: Props) {
+  const storefrontProduct = useGid<Product>(gid);
+  const firstVariant =
+    useGid<ProductVariant>(variantGid) ??
+    storefrontProduct?.variants.nodes.find(
+      (variant) => variant.id == variantGid,
+    ) ??
+    storefrontProduct?.variants.nodes[0];
+
+  if (!(storefrontProduct && firstVariant)) {
+    return null;
+  }
+
+  return (
+    <>
+      {firstVariant.image && (
+        <Image
+          className="absolute h-full w-full transform bg-cover bg-center object-cover object-center"
+          data={firstVariant.image}
+        />
+      )}
+
+      <div className="absolute bottom-4 right-4">
+        <ProductTile storefrontProduct={storefrontProduct} />
+      </div>
+    </>
+  );
+}
